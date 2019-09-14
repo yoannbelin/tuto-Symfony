ef3983fb88dac53843b2bd6a9c2eddb9afd152b5
yoannbelin <yoann.belin@icloud.com>
2019-09-14 15:23:11 +0200

    first commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenue ici les amis !",
+            'age' => 31
+        ]);
+    }
+
+    /**
+     * @Route("/blog/12", name="blog_show")
+     */
+    public function show() {
+        return $this->render('blog/show.html.twig');
+    }
+}
